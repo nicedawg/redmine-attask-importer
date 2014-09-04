8228ea974cfcb3f6303e60c1bf5e840e9a2f35ba
Brady Somerville <brady.somerville@gmail.com>
2014-09-03 22:32:12 -0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :attask_importer do
+  name 'Attask Importer plugin'
+  author 'Brady Somerville'
+  description 'Import AtTask information into Redmine'
+  version '0.0.1'
+  url 'http://github.com/nicedawg/redmine-attask-importer'
+  author_url 'http://nicedawg.net'
+end
